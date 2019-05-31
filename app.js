64981d88bd18e047488eb7b654c1ac0e139d34bf
atlansien <atlansien@yahoo.co.jp>
2019-05-31 10:34:52 +0900

    create MySQL connection

--- app.js
+++ app.js
@@ -1,6 +1,39 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const app = express();
+const PORT = process.env.PORT || 8000;
 
-app.listen(process.env.PORT || 5000, () => console.log('Example app listening on port 3000!'))
+app.set("view enjine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+
+const connection = mysql.createConnection({
+  host: "us-cdbr-iron-east-02.cleardb.net",
+  user: "b69e15cdb05a1e",
+  password: "1fe6eae5",
+  database: "heroku_43c9e0299d8e801"
+});
+
+app.get("/", (req, res) => {
+  const createTable =
+    "CREATE TABLE users (data VARCHAR(255) PRIMARY KEY,created_at TIMESTAMP DEFAULT NOW())";
+  const q = "SELECT COUNT(*) AS count FROM users";
+  connection.query(createTable, q, (err, results) => {
+    if (err) throw err;
+    const count = results[0].count;
+    res.render("home", { count: count });
+  });
+});
+
+app.get("/register", (req, res) => {
+  const data = { data: req.body.data };
+  const insert = `INSERT INTO users(data) SET ${data}`;
+  connection.query(insert, (err, results) => {
+    if (err) throw err;
+    res.redirect("/");
+  });
+});
+
+app.listen(PORT, () => console.log("Example app listening on port 3000!"));
